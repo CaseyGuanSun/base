e239a00d715554c9ddde2bd0417e8d1d46f3c974
posteritytech <guanxiansun@gmail.com>
2016-04-28 22:43:16 +0800

    add HttpClientTest.cc

---
+++ src/HttpClient/HttpClientTest.c
@@ -0,0 +1,39 @@
+/* ******************************************************
+** Author       : CaseyGuan
+** Last modified: 2016-04-28 22:27
+** Email        : caseyguan@posteritytech.com
+** Filename     : HttpClientTest.c
+** Description  : 
+** ******************************************************/
+#include "HttpClient.h"
+
+#define GET_FILE "GET_FILE"
+
+//HttpClientTest GET_FILE url  filePath
+int main(int argc, char* argv[])
+{
+	char* command = NULL;
+	char* url = NULL;
+	char* filePath = NULL;
+	int ret = 0;
+	if(argc != 4)
+	{
+		printf("Please use like this:\n");
+		printf("HttpClientTest GET_FILE url filePath\n");
+		return 0;
+	}
+
+	command = argv[1];
+	url = argv[2];
+	filePath = argv[3];
+
+	if(strcmp(command, GET_FILE))
+	{
+		ret = HttpGetFile(url, filePath);	
+	}else
+	{
+		printf("Command:%s is not supported\n", command);
+	}
+	return 0;	
+}
+
